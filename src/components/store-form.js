ee0d54f5a1c449c62ff8cb4a3a90c6c9de51d0de
Albert Cheng <lptalbert@gmail.com>
2020-08-13 05:37:40 -0700

    complete store update component

--- src/components/store-form.js
+++ src/components/store-form.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import { API, graphqlOperation } from 'aws-amplify';
 import * as mutations from '../graphql/mutations';
 import './store-form.css';
@@ -6,7 +6,7 @@ import './store-form.css';
 const initialState = {
     name: "",
     description: "",
-    phone_number: "",
+    phoneNumber: "",
     email: "",
     err: ""
 };
@@ -25,17 +25,29 @@ function reducer(state, action) {
 function StoreForm(prop) {
     const [state, dispatch] = useReducer(reducer, initialState);
 
-    async function creatStore() {
-        const { name, description, phone_number, email, err } = state;
+    useEffect(() => {
+        if (prop.action === "update") {
+            for (const key in prop.selectedStore) {
+                dispatch({ type: "SET_INPUT", key: key, value: prop.selectedStore[key] });
+            };
+            if (!prop.selectedStore.description) {
+                dispatch({ type: "SET_INPUT", key: "description", value: "" });
+            };
+            dispatch({ type: "SET_INPUT", key: "phoneNumber", value: prop.selectedStore.phoneNumber.slice(2) });
+        };
+    }, [prop]);
 
-        let phoneNumber;
-        if (!phone_number.match(/\d+/g)) {
+    async function storeAction() {
+        const { name, description, email, err } = state;
+
+        let phoneNumber = state.phoneNumber;
+        if (!phoneNumber.match(/\d+/g)) {
             phoneNumber = "";
         } else {
-            phoneNumber = "+1" + phone_number.match(/\d+/g).join("");
+            phoneNumber = "+1" + phoneNumber.match(/\d+/g).join("");
         };
 
-        if (name === "" || phone_number === "" || email === "") {
+        if (name === "" || state.phoneNumber === "" || email === "") {
             console.log("info missing");
             dispatch({ type: "SET_INPUT", key: "err", value: "info missing" });
             return;
@@ -48,23 +60,42 @@ function StoreForm(prop) {
 
         const store = { name, description, phoneNumber, email };
 
-        if (store.description === "") {
-            delete store.description;
-        };
+        if (prop.action === "create") {
+            if (store.description === "") {
+                delete store.description;
+            };
 
-        try {
-            await API.graphql(graphqlOperation(mutations.createStore, { input: store }))
-                .then(newStore => {
-                    const currentStores = prop.stores;
-                    currentStores.push(newStore.data.createStore);
-                    prop.updateStores(currentStores);
-                    console.log("store created", newStore);
-                });
-            dispatch({ type: "CLEAR_INPUT" });
-            prop.modalAction({ component: "" });
-        } catch (error) {
-            console.log("error on creating store", error);
-            dispatch({ type: "SET_INPUT", key: "err", value: error });
+            try {
+                await API.graphql(graphqlOperation(mutations.createStore, { input: store }))
+                    .then(newStore => {
+                        const currentStores = prop.stores;
+                        currentStores.push(newStore.data.createStore);
+                        prop.updateStores(currentStores);
+                        console.log("store created", newStore);
+                    });
+                dispatch({ type: "CLEAR_INPUT" });
+                prop.modalAction({ component: "" });
+            } catch (error) {
+                console.log("error on creating store", error);
+                dispatch({ type: "SET_INPUT", key: "err", value: error });
+            };
+        } else if (prop.action === "update") {
+            store.id = state.id;
+            try {
+                await API.graphql(graphqlOperation(mutations.updateStore, { input: store }))
+                    .then(newStore => {
+                        prop.updateSelectStore(newStore.data.updateStore);
+                        const currentStores = prop.stores;
+                        currentStores.splice(state.idx, 1, newStore.data.updateStore);
+                        prop.updateStores(currentStores);
+                        console.log("store updated", newStore);
+                    });
+                dispatch({ type: "CLEAR_INPUT" });
+                prop.modalAction({ component: "" });
+            } catch (error) {
+                console.log("error on updating store", error);
+                dispatch({ type: "SET_INPUT", key: "err", value: error });
+            };
         };
     };
 
@@ -74,7 +105,7 @@ function StoreForm(prop) {
 
     function handleSubmit(e) {
         e.preventDefault();
-        creatStore();
+        storeAction();
     };
 
     function close() {
@@ -83,7 +114,7 @@ function StoreForm(prop) {
 
     return (
         <div className="store-form">
-            <h3>Create New Store</h3>
+            <h3>{prop.action === "create" ? "Create New Store" : "Edit Store"}</h3>
             <form onSubmit={handleSubmit}>
                 <input
                     name='name'
@@ -98,9 +129,9 @@ function StoreForm(prop) {
                     placeholder='description'
                 />
                 <input
-                    name='phone_number'
+                    name='phoneNumber'
                     onChange={handleInput}
-                    value={state.phone_number}
+                    value={state.phoneNumber}
                     placeholder='phone number'
                 />
                 <input
@@ -109,7 +140,7 @@ function StoreForm(prop) {
                     value={state.email}
                     placeholder='email'
                 />
-                <button>Create Store</button>
+                <button>{prop.action === "create" ? "Create Store" : "Save Changes"}</button>
             </form>
             <div>{state.err}</div>
             <button onClick={close}>Cancel</button>
